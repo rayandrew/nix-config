b07a6b21e25ccfa2ce2dd4038ccc7637cd630186
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-01-11 12:47:33 -0600

    try to remove config for now

--- hosts/github-ci.nix
+++ hosts/github-ci.nix
@@ -7,14 +7,14 @@
   networking.computerName = "runner";
   networking.hostName = "runner";
 
-  config.my = {
-    username = "runner";
-    directory = {
-      nix = "/Users/runner/work/nixpkgs/nixpkgs";
-      projects = "/Users/runner/work/Projects";
-      research = "/Users/runner/work/Research";
-    };
-  };
+  # config.my = {
+  #   username = "runner";
+  #   directory = {
+  #     nix = "/Users/runner/work/nixpkgs/nixpkgs";
+  #     projects = "/Users/runner/work/Projects";
+  #     research = "/Users/runner/work/Research";
+  #   };
+  # };
 
   homebrew.enable = lib.mkForce false;
 }
