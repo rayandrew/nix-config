e86b673a9fe08887ac75bf623fdeef753a51cf69
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-01-29 12:52:39 -0600

    update format

--- packages/sketchybar-shendy/default.nix
+++ packages/sketchybar-shendy/default.nix
@@ -11,6 +11,7 @@ in stdenv.mkDerivation rec {
     repo = ".dotfiles";
     rev = "f54fc53fe70d1a8802b71f84f6472d2d872a9155";
     sha256 = "183gjqcs4gzrlcbg3qj481im8pz181cr59hl5in4dilcr16sdpd2";
+    stripRoot = false;
   };
 
   patches = [ ./patches/001-grep.patch ];
@@ -21,7 +22,7 @@ in stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    description = "SketchyBar Shendy Pratam";
+    description = "SketchyBar Shendy Pratama";
     homepage = "https://github.com/shendypratamaa/.dotfiles";
     platforms = platforms.darwin;
     maintainers = [ maintainers.rayandrew ];
