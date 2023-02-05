aadb043e4da4fde508efb044f84b35ac485d24a1
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-02-04 22:19:25 -0600

    change back the shell aliases

--- home-manager/gui.nix
+++ home-manager/gui.nix
@@ -1,14 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
-let
-  defaultModules = [
-    ./wezterm
-  ];
-  _nixosModules = [ ];
-  nixosModules = if (pkgs.stdenv.isLinux) then _nixosModules else [ ];
-  _darwinModules = [ ];
-  darwinModules = if (pkgs.stdenv.isDarwin) then _darwinModules else [ ];
-in
+# let
+#   defaultModules = [
+#     ./wezterm
+#   ];
+#   _nixosModules = [ ];
+#   nixosModules = if (pkgs.stdenv.isLinux) then _nixosModules else [ ];
+#   _darwinModules = [ ];
+#   darwinModules = if (pkgs.stdenv.isDarwin) then _darwinModules else [ ];
+# in
 {
-  imports = defaultModules ++ nixosModules ++ darwinModules;
+  imports = [ ./wezterm ];
 }
