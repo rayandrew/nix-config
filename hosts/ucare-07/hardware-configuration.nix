dd40f721e1300351acd297d865879ff6b4a8580b
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-02-05 00:05:33 -0600

    add new mountpoints

--- hosts/ucare-07/hardware-configuration.nix
+++ hosts/ucare-07/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
@@ -13,14 +14,31 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/7e1e2919-a081-493f-a653-542eb7892bc0";
+  fileSystems = {
+    # 500GB SSD
+    "/" = {
+      device = "/dev/disk/by-uuid/7e1e2919-a081-493f-a653-542eb7892bc0";
       fsType = "ext4";
     };
+    # 1TB SSD
+    "/mnt/ssd-1" = {
+      device = "/dev/disk/by-uuid/8601910d-9fe8-4b5f-9d3a-5e4c949bf71c";
+      fsType = "ext4";
+    };
+    # 1TB HDD
+    "/mnt/hdd-1" = {
+      device = "/dev/disk/by-uuid/25583dce-823c-4e45-a793-273b93651e4a";
+      fsType = "ext4";
+    };
+    # 120GB HDD
+    "/mnt/hdd-2" = {
+      device = "/dev/disk/by-uuid/6fdb11fc-1584-4e5d-ae9a-db4766987935";
+      fsType = "ext4";
+    };
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/032dc53d-4a0c-4431-a2ca-e94301e2f84b"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/032dc53d-4a0c-4431-a2ca-e94301e2f84b"; }];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
