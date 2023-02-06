06a1251fde5b8ba4ae08b13d6d67a981813e9e41
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-02-06 11:55:04 -0600

    remove `signing` ssh for now

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -16,12 +16,12 @@
     ignores = [ ".DS_Store" ];
 
     aliases = { };
-    signing = {
-      # key = "E2E8D63137DD489E";
-      # key =
-      #   "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMHAzQAtsfWptVuemoBzWLwqdLYTkGNGFiihhXq6Qi3EZk94jpmD2YM/UxzQVgbBCALSaKdTLRiwAfy2C+0sIh8XomiFqgnZ96K+FjSkSkcz9IIL6m8VqCNTcCElK8C5u+MSr611v2giYrJbgEJSdK7assp348B3E2p4opZ5OxYXHL8EOj/5pWjUjLa0CK1SQQJbdjCfw4kqrXelgT8iwVERIU2WO7YlEOgWWEDS7pOseYIHcf77zzcAb6P1b6iEvDvvKuooN0mSZtkOdMmTnEUTKkQQPhkuuMId1GFXl6Y6XscLfxO0fag9pC4QkB5SrgYPX8wCOm+N4ihk3Ok2r5";
-      # signByDefault = true;
-    };
+    # signing = {
+    # key = "E2E8D63137DD489E";
+    # key =
+    #   "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMHAzQAtsfWptVuemoBzWLwqdLYTkGNGFiihhXq6Qi3EZk94jpmD2YM/UxzQVgbBCALSaKdTLRiwAfy2C+0sIh8XomiFqgnZ96K+FjSkSkcz9IIL6m8VqCNTcCElK8C5u+MSr611v2giYrJbgEJSdK7assp348B3E2p4opZ5OxYXHL8EOj/5pWjUjLa0CK1SQQJbdjCfw4kqrXelgT8iwVERIU2WO7YlEOgWWEDS7pOseYIHcf77zzcAb6P1b6iEvDvvKuooN0mSZtkOdMmTnEUTKkQQPhkuuMId1GFXl6Y6XscLfxO0fag9pC4QkB5SrgYPX8wCOm+N4ihk3Ok2r5";
+    # signByDefault = true;
+    # };
     extraConfig = {
       init = { defaultBranch = config.my-meta.mainBranch; };
       gpg = { format = "ssh"; };
