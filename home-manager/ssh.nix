d229a7f2d14671dd9b1f2cdbcab7ca41350f10dc
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-02-10 21:09:11 -0600

    disable 1password ssh agent for now

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -67,12 +67,12 @@ in
     Match host * exec "test -z $SSH_TTY"
       IdentityFile "${config.home.homeDirectory}/.ssh/id_ed25519"
       ''}
-      ${optionalString pkgs.stdenv.isDarwin ''
-    # Only set `IdentityAgent` not connected remotely via SSH.
-    # This allows using agent forwarding when connecting remotely.
-    Match host * exec "test -z $SSH_TTY"
-      IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-      ''}
+    #   ${optionalString pkgs.stdenv.isDarwin ''
+    # # Only set `IdentityAgent` not connected remotely via SSH.
+    # # This allows using agent forwarding when connecting remotely.
+    # Match host * exec "test -z $SSH_TTY"
+    #   IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    #   ''}
   '';
 
   home.packages = [
