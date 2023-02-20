f653648976f6a440eda7e85c826b2a75b1673279
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-02-19 22:21:06 -0600

    update flake

--- home-manager/wezterm/default.nix
+++ home-manager/wezterm/default.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.wezterm = {
     enable = true;
     colorSchemes = { };
+    # package = pkgs.stable.wezterm;
     extraConfig = builtins.readFile ./config.lua;
   };
 }
