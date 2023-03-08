676bc22bcabe15ad9b28a16663fe56876edb1127
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-03-07 23:02:58 -0600

    update flakes

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -1,33 +1,5 @@
 { config, lib, pkgs, ... }:
 
-let
-  marketplaceExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      name = "tokyo-night";
-      publisher = "enkia";
-      version = "0.9.6";
-      sha256 = "sha256-Vk6wIGMzWPpv+A4vnHWAYnxTFYQBpVYZNu1BRim/TN0=";
-    }
-    {
-      name = "black-formatter";
-      publisher = "ms-python";
-      version = "2022.7.13271013";
-      sha256 = "sha256-wXAIPrk52L9xZNY3bitMUaUNl5q0iCNmRKK+Z/ZHmsU=";
-    }
-    {
-      name = "copilot";
-      publisher = "GitHub";
-      version = "1.73.8685";
-      sha256 = "sha256-W1j1VAuSM1sgxHRIahqVncUlknT+MPi7uutY+0NURZQ=";
-    }
-    {
-      name = "ayu";
-      publisher = "teabyii";
-      version = "1.0.5";
-      sha256 = "sha256-+IFqgWliKr+qjBLmQlzF44XNbN7Br5a119v9WAnZOu4=";
-    }
-  ];
-in
 {
   programs.vscode = {
     enable = true;
@@ -42,7 +14,32 @@ in
         ms-azuretools.vscode-docker
         ms-vscode-remote.remote-ssh
         alefragnani.project-manager
-      ] ++ marketplaceExtensions;
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "tokyo-night";
+          publisher = "enkia";
+          version = "0.9.6";
+          sha256 = "sha256-Vk6wIGMzWPpv+A4vnHWAYnxTFYQBpVYZNu1BRim/TN0=";
+        }
+        {
+          name = "black-formatter";
+          publisher = "ms-python";
+          version = "2022.7.13271013";
+          sha256 = "sha256-wXAIPrk52L9xZNY3bitMUaUNl5q0iCNmRKK+Z/ZHmsU=";
+        }
+        {
+          name = "copilot";
+          publisher = "GitHub";
+          version = "1.73.8685";
+          sha256 = "sha256-W1j1VAuSM1sgxHRIahqVncUlknT+MPi7uutY+0NURZQ=";
+        }
+        {
+          name = "ayu";
+          publisher = "teabyii";
+          version = "1.0.5";
+          sha256 = "sha256-+IFqgWliKr+qjBLmQlzF44XNbN7Br5a119v9WAnZOu4=";
+        }
+      ];
     userSettings =
       let
         fontSize = 18;
