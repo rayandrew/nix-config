97c1688b143c26d00b77e0ebbfdb4c4d6f779a02
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-03-18 00:02:50 -0500

    add mustache templating

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -51,108 +51,18 @@ in
     viAlias = true;
     vimAlias = true;
     defaultEditor = true;
-    plugins = with pkgs; [
-      # vimPlugins.lazy-nvim
-      # vimPlugins.gruvbox-nvim
-    ];
-    extraLuaConfig = with pkgs; ''
-      vim.opt.rtp:prepend("${vimPlugins.lazy-nvim}")
-
-      vim.g.mapleader = " "
-      vim.o.background = "dark"
-
-      require("lazy").setup({
-        { 
-          dir = "${vimPlugins.gruvbox-nvim}",  
-          lazy = false,
-          priority = 1000,
-          name = "gruvbox",
-          config = function()
-            vim.cmd([[colorscheme gruvbox]])
-          end,
-        },
-        { 
-          dir = "${vimPlugins.plenary-nvim}", 
-          lazy = true,
-        },
-        {
-          dir = "${vimPlugins.telescope-nvim}",
-          cmd = "Telescope",
-        }
-      }, {
-        install = {
-          colorscheme = { "gruvbox" },
-        },
-        -- load the default settings
-        defaults = {
-          autocmds = true, -- lazyvim.config.autocmds
-          keymaps = true, -- lazyvim.config.keymaps
-          options = true, -- lazyvim.config.options
-        },
-        performance = {
-          cache = {
-            enabled = true,
-          },
-          reset_packpath = true, -- reset the package path to improve startup time
-          rtp = {
-            reset = true,
-          }
-        },
-        ui = {
-          -- icons used by other plugins
-          icons = {
-            diagnostics = {
-              Error = " ",
-              Warn = " ",
-              Hint = " ",
-              Info = " ",
-            },
-            git = {
-              added = " ",
-              modified = " ",
-              removed = " ",
-            },
-            kinds = {
-              Array = " ",
-              Boolean = " ",
-              Class = " ",
-              Color = " ",
-              Constant = " ",
-              Constructor = " ",
-              Copilot = " ",
-              Enum = " ",
-              EnumMember = " ",
-              Event = " ",
-              Field = " ",
-              File = " ",
-              Folder = " ",
-              Function = " ",
-              Interface = " ",
-              Key = " ",
-              Keyword = " ",
-              Method = " ",
-              Module = " ",
-              Namespace = " ",
-              Null = " ",
-              Number = " ",
-              Object = " ",
-              Operator = " ",
-              Package = " ",
-              Property = " ",
-              Reference = " ",
-              Snippet = " ",
-              String = " ",
-              Struct = " ",
-              Text = " ",
-              TypeParameter = " ",
-              Unit = " ",
-              Value = " ",
-              Variable = " ",
-            },
-          },
-        }
-      })
-    '';
+    # plugins = with pkgs; [
+    # vimPlugins.lazy-nvim
+    # vimPlugins.gruvbox-nvim
+    # ];
+    extraLuaConfig = builtins.readFile (templateFile "init.lua" ./config/init.lua {
+      plugins = with pkgs; {
+        lazy = vimPlugins.lazy-nvim;
+        gruvbox = vimPlugins.gruvbox-nvim;
+        telescope = vimPlugins.telescope-nvim;
+        plenary = vimPlugins.plenary-nvim;
+      };
+    });
   };
 
   # xdg.configFile."nvim" = {
