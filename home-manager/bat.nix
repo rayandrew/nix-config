6dca03c10f8adc626e14f7ea10fdd04c580f1ebf
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-05-09 20:02:04 -0500

    use `bat.extraPackages` only for `darwin`

--- home-manager/bat.nix
+++ home-manager/bat.nix
@@ -7,25 +7,32 @@ let
   flavor = "mocha";
 in
 {
-  programs.bat = {
-    enable = true;
-    config = {
-      theme = "catppuccin-${flavor}";
-    };
-    extraPackages = with pkgs.bat-extras; lib.mkIf (pkgs.stdenv.isDarwin) [
-      batdiff
-      batman
-      batgrep
-      batwatch
-    ];
-    themes = {
-      "catppuccin-${flavor}" = builtins.readFile (pkgs.fetchFromGitHub
-        {
-          owner = "catppuccin";
-          repo = "bat"; # Bat uses sublime syntax for its themes
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-          sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-        } + "/Catppuccin-${flavor}.tmTheme");
-    };
-  };
+  config = (lib.mkMerge [
+    {
+      programs.bat = {
+        enable = true;
+        config = {
+          theme = "catppuccin-${flavor}";
+        };
+        themes = {
+          "catppuccin-${flavor}" = builtins.readFile (pkgs.fetchFromGitHub
+            {
+              owner = "catppuccin";
+              repo = "bat"; # Bat uses sublime syntax for its themes
+              rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+              sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+            } + "/Catppuccin-${flavor}.tmTheme");
+        };
+      };
+    }
+    (lib.mkIf pkgs.stdenv.isDarwin {
+      programs.bat.extraPackages = with pkgs.bat-extras;  [
+        batdiff
+        batman
+        batgrep
+        batwatch
+      ];
+    })
+  ]);
+
 }
