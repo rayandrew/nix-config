4a08be1c26b878e5f7c2d38ecc387a3f1d035eea
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-06-08 23:21:01 -0500

    add `wiki` hosts

---
+++ hosts/wiki/default.nix
@@ -0,0 +1,87 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, flake, ... }:
+
+let
+  inherit (flake) inputs;
+  inherit (config.my-meta) username;
+  inherit (config.users.users.${username}) home;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../nixos/minimal.nix
+    ../../nixos/security.nix
+    ../../nixos/ssh.nix
+    ../../nixos/vps.nix
+    inputs.hardware.nixosModules.common-cpu-intel
+  ];
+
+  device = {
+    type = "server";
+    netDevices = [ "eth0" "enp7s0" ];
+  };
+
+  boot.cleanTmpDir = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
+
+  networking = {
+    hostName = "wiki";
+    nameservers = [
+      "8.8.8.8"
+    ];
+    defaultGateway = "172.31.1.1";
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "eth0";
+    };
+    dhcpcd.enable = false;
+    usePredictableInterfaceNames = lib.mkForce false;
+    interfaces = {
+      eth0 = {
+        ipv4.addresses = [
+          { address = "5.161.212.0"; prefixLength = 32; }
+        ];
+        ipv6.addresses = [
+          { address = "2a01:4ff:f0:3e54::1"; prefixLength = 64; }
+          { address = "fe80::9400:2ff:fe43:4353"; prefixLength = 64; }
+        ];
+        ipv4.routes = [{ address = "172.31.1.1"; prefixLength = 32; }];
+        ipv6.routes = [{ address = "fe80::1"; prefixLength = 128; }];
+      };
+    };
+    firewall.allowedTCPPorts = [
+      80
+      443
+      993 # imap ssl
+      465 # smtp ssl
+    ];
+  };
+
+  services.udev.extraRules = ''
+    ATTR{address}=="96:00:02:43:43:53", NAME="eth0"
+  '';
+
+  zramSwap.enable = true;
+
+  my-meta.nixConfigPath = "${home}/.config/nix-config";
+  my-meta.projectsDirPath = "";
+  my-meta.researchDirPath = "";
+
+  security.pam.enableSSHAgentAuth = true; # to make deploy-rs works
+
+  users.users.${username}.shell = lib.mkForce pkgs.bash;
+
+  services.smartd.enable = lib.mkForce false;
+
+  environment.systemPackages = with pkgs;[ unzip ];
+
+  # secrets
+  sops.secrets = { };
+}
