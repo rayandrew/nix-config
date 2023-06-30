fb01be915fae852bc3ee532dd4d6fad63b65a188
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-06-29 21:04:11 -0500

    change locale

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -26,6 +26,6 @@
     layout = lib.mkDefault "us";
     xkbVariant = lib.mkDefault "intl";
     # Remap Caps Lock to Esc, and use Super+Space to change layouts
-    xkbOptions = lib.mkDefault "caps:escape,grp:win_space_toggle";
+    # xkbOptions = lib.mkDefault "caps:escape,grp:win_space_toggle";
   };
 }
