6d276da3d66e73c0b054540f186f6278e111c050
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-06-29 19:39:40 -0500

    remove bars

--- home-manager/sway/default.nix
+++ home-manager/sway/default.nix
@@ -14,7 +14,7 @@ let
     import ../i3/common.nix rec {
       inherit config lib modifier alt dunstctl;
       browser = "firefox";
-      bars = [{ command = "${config.programs.waybar.package}/bin/waybar"; }];
+      # bars = [{ command = "${config.programs.waybar.package}/bin/waybar"; }];
       fileManager = "${terminal} ${config.programs.nnn.finalPackage}/bin/nnn -a -P p";
       menu = "${config.programs.fuzzel.package}/bin/fuzzel";
       # light needs to be installed in system, so not defining a path here
