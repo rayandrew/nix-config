8d5b424ad29e69d8a968e6c7db57cbfb70c388f9
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-07-30 18:41:04 -0500

    upgrade flake

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -159,207 +159,207 @@ in
           sha256 = "sha256-+IFqgWliKr+qjBLmQlzF44XNbN7Br5a119v9WAnZOu4=";
         }
       ];
-    userSettings =
-      {
-        "telemetry.telemetryLevel" = "off";
-
-        "update.mode" = "none";
-
-        # "workbench.colorTheme" = "Ayu Light";
-        # "workbench.colorTheme" = "Gruvbox Light Soft";
-        # "workbench.colorTheme" = "Gruvbox Dark Medium";
-        # "workbench.colorTheme" = "GitHub Light High Contrast";
-        # "workbench.colorTheme" = "Catppuccin Mocha";
-        # "workbench.colorTheme" = "Catppuccin Latte";
-        "workbench.colorTheme" = "Catppuccin Macchiato";
-        # "workbench.colorTheme" = "Default Light+";
-        "workbench.startupEditor" = "none";
-        "workbench.editor.enablePreview" = false;
-        # "workbench.list.openMode" = "doubleClick";
-        # "workbench.activityBar.visible" = false;
-        # "workbench.statusBar.visible" = false;
-        # "workbench.layoutControl.enabled" = false;
-        # "workbench.layoutControl.type" = "menu";
-        # "workbench.editor.limit.enabled" = true;
-        # "workbench.editor.limit.value" = 1;
-        # "workbench.editor.limit.perEditorGroup" = true;
-        # "workbench.editor.showTabs" = false;
-        # "workbench.editor.enablePreview" = false;
-
-        "breadcrumbs.enabled" = false;
-        "explorer.openEditors.visible" = 0;
-        "window.commandCenter" = true;
-
-        # editor
-        # "editor.fontFamily" = "Pragmata Pro Mono";
-        "editor.renderControlCharacters" = false;
-        # "editor.fontFamily" = "JetBrainsMono Nerd Font Mono";
-        "editor.fontFamily" = "UbuntuMono Nerd Font Mono";
-        "editor.fontSize" = fontSize;
-        "editor.wordWrap" = "on";
-        "editor.minimap.enabled" = false;
-        "editor.lineNumbers" = "relative";
-        "editor.scrollbar.verticalScrollbarSize" = 2;
-        "editor.scrollbar.horizontalScrollbarSize" = 2;
-        "editor.scrollbar.vertical" = "hidden";
-        "editor.scrollbar.horizontal" = "hidden";
-        "editor.accessibilitySupport" = "off";
-
-        # integrated terminal
-        "terminal.integrated.fontSize" = fontSize;
-        "terminal.integrated.allowChords" = false;
-
-        # python
-        "python.formatting.provider" = "none";
-        "[python]" = {
-          "editor.defaultFormatter" = "ms-python.black-formatter";
-          "editor.formatOnSave" = true;
-          "editor.codeActionsOnSave" = {
-            "source.organizeImports" = true;
-          };
-        };
-        "isort.args" = [ "--profile" "black" ];
-
-        # vim 
-        "vim.disableExtension" = false;
-        "vim.easymotion" = true;
-        "vim.incsearch" = true;
-        "vim.useSystemClipboard" = true;
-        "vim.useCtrlKeys" = true;
-        "vim.hlsearch" = true;
-        "vim.insertModeKeyBindings" = [
-          {
-            before = [ "j" "j" ];
-            after = [ "<Esc>" ];
-          }
-        ];
-        "vim.normalModeKeyBindingsNonRecursive" = [
-          {
-            before = [ "<leader>" "d" ];
-            after = [ "d" "d" ];
-          }
-          {
-            before = [ "j" ];
-            after = [ "g" "j" ];
-          }
-          {
-            before = [ "k" ];
-            after = [ "g" "k" ];
-          }
-          {
-            before = [ "<C-n>" ];
-            commands = [ ":nohl" ];
-          }
-          {
-            before = [ "K" ];
-            commands = [ "editor.action.peekDefinition" ];
-          }
-          {
-            before = [ "<leader>" "f" "s" ];
-            commands = [ ":w" ];
-          }
-          {
-            before = [ "<leader>" "e" ];
-            commands = [ "workbench.action.toggleSidebarVisibility" ];
-          }
-          {
-            before = [ "<leader>" "s" "p" ];
-            commands = [ "find-it-faster.findWithinFiles" ];
-          }
-          {
-            before = [ "<leader>" "s" "/" ];
-            commands = [ "find-it-faster.findWithinFiles" ];
-          }
-          {
-            before = [ "<leader>" "f" "f" ];
-            commands = [ "find-it-faster.findFiles" ];
-          }
-          {
-            before = [ "<leader>" "w" "v" ];
-            after = [ "<C-w>" "v" ];
-          }
-          {
-            before = [ "<leader>" "w" "s" ];
-            after = [ "<C-w>" "s" ];
-          }
-          {
-            before = [ "<leader>" "w" "q" ];
-            after = [ "<C-w>" "q" ];
-          }
-          {
-            before = [ "<C-j>" ];
-            after = [ "<C-w>" "j" ];
-          }
-          {
-            before = [ "<leader>" "w" "h" ];
-            after = [ "<C-w>" "h" ];
-          }
-          {
-            before = [ "<C-j>" ];
-            after = [ "<C-w>" "j" ];
-          }
-          {
-            before = [ "<leader>" "w" "j" ];
-            after = [ "<C-w>" "j" ];
-          }
-          {
-            before = [ "<C-k>" ];
-            after = [ "<C-w>" "k" ];
-          }
-          {
-            before = [ "<leader>" "w" "k" ];
-            after = [ "<C-w>" "k" ];
-          }
-          {
-            before = [ "<C-l>" ];
-            after = [ "<C-w>" "l" ];
-          }
-          {
-            before = [ "<leader>" "w" "l" ];
-            after = [ "<C-w>" "l" ];
-          }
-          {
-            before = [ "<leader>" "b" "d" ];
-            commands = [ ":bd" ];
-          }
-          {
-            before = [ "<leader>" "b" "b" ];
-            commands = [ "workbench.action.openPreviousEditorFromHistory" ];
-          }
-        ];
-        "vim.leader" = "<space>";
-        "vim.handleKeys" = {
-          "<C-a>" = false;
-          "<C-f>" = false;
-        };
-      };
-    keybindings = [
-      {
-        key = "enter";
-        command = "explorer.openAndPassFocus";
-        when = "filesExplorerFocus && foldersViewVisible && !explorerResourceIsFolder && !inputFocus";
-      }
-      {
-        # key = "shift+enter";
-        key = "r";
-        command = "renameFile";
-        when = "filesExplorerFocus && foldersViewVisible && !explorerResourceIsRoot && !explorerResourceReadonly && !inputFocus";
-      }
-      {
-        key = "tab";
-        command = "togglePeekWidgetFocus";
-        when = "inReferenceSearchEditor && vim.active && vim.mode != 'Insert' || referenceSearchVisible";
-      }
-      {
-        key = "ctrl+tab";
-        command = "workbench.action.openPreviousEditorFromHistory";
-      }
-      {
-        key = "ctrl+tab";
-        command = "workbench.action.quickOpenNavigateNext";
-        "when" = "inQuickOpen";
-      }
-    ] ++ generateNeovimKeybinding;
+    # userSettings =
+    #   {
+    #     "telemetry.telemetryLevel" = "off";
+    #
+    #     "update.mode" = "none";
+    #
+    #     # "workbench.colorTheme" = "Ayu Light";
+    #     # "workbench.colorTheme" = "Gruvbox Light Soft";
+    #     # "workbench.colorTheme" = "Gruvbox Dark Medium";
+    #     # "workbench.colorTheme" = "GitHub Light High Contrast";
+    #     # "workbench.colorTheme" = "Catppuccin Mocha";
+    #     # "workbench.colorTheme" = "Catppuccin Latte";
+    #     "workbench.colorTheme" = "Catppuccin Macchiato";
+    #     # "workbench.colorTheme" = "Default Light+";
+    #     "workbench.startupEditor" = "none";
+    #     "workbench.editor.enablePreview" = false;
+    #     # "workbench.list.openMode" = "doubleClick";
+    #     # "workbench.activityBar.visible" = false;
+    #     # "workbench.statusBar.visible" = false;
+    #     # "workbench.layoutControl.enabled" = false;
+    #     # "workbench.layoutControl.type" = "menu";
+    #     # "workbench.editor.limit.enabled" = true;
+    #     # "workbench.editor.limit.value" = 1;
+    #     # "workbench.editor.limit.perEditorGroup" = true;
+    #     # "workbench.editor.showTabs" = false;
+    #     # "workbench.editor.enablePreview" = false;
+    #
+    #     "breadcrumbs.enabled" = false;
+    #     "explorer.openEditors.visible" = 0;
+    #     "window.commandCenter" = true;
+    #
+    #     # editor
+    #     # "editor.fontFamily" = "Pragmata Pro Mono";
+    #     "editor.renderControlCharacters" = false;
+    #     # "editor.fontFamily" = "JetBrainsMono Nerd Font Mono";
+    #     "editor.fontFamily" = "UbuntuMono Nerd Font Mono";
+    #     "editor.fontSize" = fontSize;
+    #     "editor.wordWrap" = "on";
+    #     "editor.minimap.enabled" = false;
+    #     "editor.lineNumbers" = "relative";
+    #     "editor.scrollbar.verticalScrollbarSize" = 2;
+    #     "editor.scrollbar.horizontalScrollbarSize" = 2;
+    #     "editor.scrollbar.vertical" = "hidden";
+    #     "editor.scrollbar.horizontal" = "hidden";
+    #     "editor.accessibilitySupport" = "off";
+    #
+    #     # integrated terminal
+    #     "terminal.integrated.fontSize" = fontSize;
+    #     "terminal.integrated.allowChords" = false;
+    #
+    #     # python
+    #     "python.formatting.provider" = "none";
+    #     "[python]" = {
+    #       "editor.defaultFormatter" = "ms-python.black-formatter";
+    #       "editor.formatOnSave" = true;
+    #       "editor.codeActionsOnSave" = {
+    #         "source.organizeImports" = true;
+    #       };
+    #     };
+    #     "isort.args" = [ "--profile" "black" ];
+    #
+    #     # vim 
+    #     "vim.disableExtension" = false;
+    #     "vim.easymotion" = true;
+    #     "vim.incsearch" = true;
+    #     "vim.useSystemClipboard" = true;
+    #     "vim.useCtrlKeys" = true;
+    #     "vim.hlsearch" = true;
+    #     "vim.insertModeKeyBindings" = [
+    #       {
+    #         before = [ "j" "j" ];
+    #         after = [ "<Esc>" ];
+    #       }
+    #     ];
+    #     "vim.normalModeKeyBindingsNonRecursive" = [
+    #       {
+    #         before = [ "<leader>" "d" ];
+    #         after = [ "d" "d" ];
+    #       }
+    #       {
+    #         before = [ "j" ];
+    #         after = [ "g" "j" ];
+    #       }
+    #       {
+    #         before = [ "k" ];
+    #         after = [ "g" "k" ];
+    #       }
+    #       {
+    #         before = [ "<C-n>" ];
+    #         commands = [ ":nohl" ];
+    #       }
+    #       {
+    #         before = [ "K" ];
+    #         commands = [ "editor.action.peekDefinition" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "f" "s" ];
+    #         commands = [ ":w" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "e" ];
+    #         commands = [ "workbench.action.toggleSidebarVisibility" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "s" "p" ];
+    #         commands = [ "find-it-faster.findWithinFiles" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "s" "/" ];
+    #         commands = [ "find-it-faster.findWithinFiles" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "f" "f" ];
+    #         commands = [ "find-it-faster.findFiles" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "w" "v" ];
+    #         after = [ "<C-w>" "v" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "w" "s" ];
+    #         after = [ "<C-w>" "s" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "w" "q" ];
+    #         after = [ "<C-w>" "q" ];
+    #       }
+    #       {
+    #         before = [ "<C-j>" ];
+    #         after = [ "<C-w>" "j" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "w" "h" ];
+    #         after = [ "<C-w>" "h" ];
+    #       }
+    #       {
+    #         before = [ "<C-j>" ];
+    #         after = [ "<C-w>" "j" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "w" "j" ];
+    #         after = [ "<C-w>" "j" ];
+    #       }
+    #       {
+    #         before = [ "<C-k>" ];
+    #         after = [ "<C-w>" "k" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "w" "k" ];
+    #         after = [ "<C-w>" "k" ];
+    #       }
+    #       {
+    #         before = [ "<C-l>" ];
+    #         after = [ "<C-w>" "l" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "w" "l" ];
+    #         after = [ "<C-w>" "l" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "b" "d" ];
+    #         commands = [ ":bd" ];
+    #       }
+    #       {
+    #         before = [ "<leader>" "b" "b" ];
+    #         commands = [ "workbench.action.openPreviousEditorFromHistory" ];
+    #       }
+    #     ];
+    #     "vim.leader" = "<space>";
+    #     "vim.handleKeys" = {
+    #       "<C-a>" = false;
+    #       "<C-f>" = false;
+    #     };
+    #   };
+    # keybindings = [
+    #   {
+    #     key = "enter";
+    #     command = "explorer.openAndPassFocus";
+    #     when = "filesExplorerFocus && foldersViewVisible && !explorerResourceIsFolder && !inputFocus";
+    #   }
+    #   {
+    #     # key = "shift+enter";
+    #     key = "r";
+    #     command = "renameFile";
+    #     when = "filesExplorerFocus && foldersViewVisible && !explorerResourceIsRoot && !explorerResourceReadonly && !inputFocus";
+    #   }
+    #   {
+    #     key = "tab";
+    #     command = "togglePeekWidgetFocus";
+    #     when = "inReferenceSearchEditor && vim.active && vim.mode != 'Insert' || referenceSearchVisible";
+    #   }
+    #   {
+    #     key = "ctrl+tab";
+    #     command = "workbench.action.openPreviousEditorFromHistory";
+    #   }
+    #   {
+    #     key = "ctrl+tab";
+    #     command = "workbench.action.quickOpenNavigateNext";
+    #     "when" = "inQuickOpen";
+    #   }
+    # ] ++ generateNeovimKeybinding;
 
   };
 }
